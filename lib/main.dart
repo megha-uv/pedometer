38e9da97a39a9769018d6d5b51e8b26b7477ad64
Megha Vadivelu <meghauvadivelu@gmail.com>
2022-03-24 14:51:50 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:untitled/dahboard.dart';
+
+import 'package:untitled/footCounter.dart';
+
+void main() => runApp(
+      const MaterialApp(
+        home: dashboard(),
+        debugShowCheckedModeBanner: false,
+        title: "Foot step Counter",
+      ),
+    );
